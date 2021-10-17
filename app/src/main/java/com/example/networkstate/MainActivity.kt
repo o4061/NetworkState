475529c754e7a5e5d0ec81d715925b4a0aae9efb
orestis faltakas <faltakas@ics.forth.gr>
2021-10-17 18:08:50 +0300

    + check Network state with livedata

--- app/src/main/java/com/example/networkstate/MainActivity.kt
+++ app/src/main/java/com/example/networkstate/MainActivity.kt
@@ -1,35 +1,23 @@
 package com.example.networkstate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var networkListener: NetworkListener
-    private lateinit var network: Network
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NetworkLiveData.init(application)
 
-        checkBtn.setOnClickListener {
-            network = Network()
-            //network.checkNetworkAvailability(this)
-            Toast.makeText(this@MainActivity, network.checkNetworkAvailability(this).toString(), Toast.LENGTH_SHORT).show()
-        }
-        
+        NetworkLiveData.observe(this, Observer {
+            if (it) {
+                Log.d("aaaaaaaaaaaaaa", "Connected")
+            } else {
+                Log.d("aaaaaaaaaaaaaa", "Connection Lost")
+            }
+        })
 
-//        lifecycleScope.launch {
-//            networkListener = NetworkListener()
-//            networkListener.checkNetworkAvailability(this@MainActivity)
-//                .collect { status ->
-//                    Toast.makeText(this@MainActivity, status.toString(), Toast.LENGTH_SHORT).show()
-//                }
-//        }
     }
 }
